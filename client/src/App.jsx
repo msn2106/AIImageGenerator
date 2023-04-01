f689faf567d53ad981e9f24186b960ea718aac5a
Mayank Singh <msingh@cogitate.us>
2023-04-02 02:01:38 +0530

    ref#102 - Add React Toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,40 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { logo } from "./assets";
 import { CreatePost, Home } from "./pages";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
-        <Link to='/'>
-          <img src={logo} alt='logo' className='w-28 object-contain' />
-        </Link>
-        <Link to='/post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
-          Create
-        </Link>
-      </header>
-      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/create-post' element={<CreatePost />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+          <Link to='/'>
+            <img src={logo} alt='logo' className='w-28 object-contain' />
+          </Link>
+          <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>
+            Create
+          </Link>
+        </header>
+        <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/create-post' element={<CreatePost />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+      <ToastContainer
+        position='bottom-right'
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 };
 
