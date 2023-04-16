68327596db534f12e52bfe51fe74fd93895af313
Mayank Singh <msingh@cogitate.us>
2023-04-16 14:00:13 +0530

    Increased the size of generated image box and viteconfig for basepath

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -102,7 +102,7 @@ const CreatePost = () => {
             handleSurpriseMe={handleSurpriseMe}
           />
 
-          <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+          <div className='relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-150 p-3 h-150 flex justify-center items-center'>
             {form.photo ? (
               <img src={form.photo} alt={form.prompt} className='w-full h-full object-contain' />
             ) : (
